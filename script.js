d16604715b146dc585b755f4f7795b8702c1d316
Baker Al-Nakib <bakuur@gmail.com>
2025-05-01 02:57:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function handleFile(e) {
   const file = e.target.files[0];
   const reader = new FileReader();
 
+  
   reader.onload = function(event) {
     const data = new Uint8Array(event.target.result);
     const workbook = XLSX.read(data, { type: "array" });
